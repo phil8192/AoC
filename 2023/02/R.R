10d1c1a50e7b1914abd3549c96bc3a96df642fb2
phil8192 <phil@parasec.net>
2024-04-10 22:33:33 +0000

    2

---
+++ 2023/02/R.R
@@ -0,0 +1,23 @@
+x <- readLines("input.txt")
+
+# ffs.
+df <- cbind(id=1:length(x), do.call(rbind, lapply(x, function(v) {
+  sets <- strsplit(sub("^.*:", "", v), ";")
+  rgb <- list(red=0, green=0, blue=0)
+  for(set in sets) {
+    cubes <- sub(" ", "", unlist(strsplit(set, ",")))
+    for(cube in cubes) {
+      cube <- unlist(strsplit(cube, " "))
+      col <- cube[2]
+      val <- cube[1]
+      rgb[col] <- max(rgb[[col]], as.integer(val))
+    }
+  }
+  data.frame(rgb)
+})))
+
+# 1: 2593
+sum(df[with(df, red <= 12 & green <= 13 & blue <= 14), ]$id)
+
+# 2. 54699
+sum(apply(df[, -1], 1, prod))
---
+++ 2023/02/README.md
@@ -0,0 +1,11 @@
+# [day 2](https://adventofcode.com/2023/day/2)
+
+```
+[phil@arasaka 02]$ time Rscript R.R
+[1] 2593
+[1] 54699
+
+real	0m0.157s
+user	0m0.142s
+sys	0m0.016s
+```
